76b025b62cce407aef60eab8823fbffef3432873
Ezell <iamezell@gmail.com>
2015-10-26 16:04:32 -0400

    adding gulp tasks and such

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+gulp.task('default', function(){
+	//place code in here
+	return browserify('src/js/app.js')
+	.bundle()
+	//Pass desired output filename to vinyl-source-stream
+	.pipe(source('bundle.js'))
+	.pipe(gulp.dest('./public/javascripts'))
+})
